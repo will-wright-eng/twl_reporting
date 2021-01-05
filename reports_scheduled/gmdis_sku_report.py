20a32d32712f77250a6c3185c3885ae1cecc66f7
william-cass-wright <william.cass.wright@gmail.com>
2021-01-04 22:24:16 -0700

    first commit

--- reports_scheduled/gmdis_sku_report.py
+++ reports_scheduled/gmdis_sku_report.py
@@ -21,33 +21,16 @@ import numpy as np
 import my_utils
 
 
-def gmdis_check(ele):
-    if 'GMDIS' in ele:
-        return True
-    else:
-        return False
-
-
 def products_to_dict(ele):
     x = [i.split(',') for i in ele.split('|')]
     return [{i.split(':')[0]: i.split(':')[1]
              for i in j if ':' in i} for j in x]
 
 
-def run():
-    today = str(dt.datetime.today()).split(' ')[0]
-
-    config = my_utils.import_configs()
-    config_dict = dict(config.items('dir_info'))
-    path = config_dict['gen_path']
-    order_path = my_utils.most_recent_order_file(path)
-    df = pd.read_csv(order_path)
-
-    df['product_details_bool'] = df['Product Details'].apply(
-        lambda x: gmdis_check(x))
-    ndf = df.loc[df['product_details_bool'] == True]
-
-    order_cols = ['Order ID', 'Order Status', 'Order Date']
+def split_to_order_lines(ndf, order_cols):
+    '''docstring
+    order_cols: retain these columns from the order row, fanning out into the order lines table
+    '''
     dfs = []
     for row in range(len(ndf)):
         row_dict = {col: ndf.iloc[row][col] for col in order_cols}
@@ -60,12 +43,40 @@ def run():
 
     df = pd.concat(dfs)
     df.columns = [i.strip() for i in list(df)]
+    return df
+
+
+def run():
+    today = str(dt.datetime.today()).split(' ')[0]
+
+    config = my_utils.import_configs()
+    config_dict = dict(config.items('dir_info'))
+    path = config_dict['gen_path']
+    order_path = my_utils.most_recent_order_file(path)
+    print(order_path)
+    df = pd.read_csv(order_path)
+    df['Order Date'] = pd.to_datetime(df['Order Date'])
+    df = df.loc[df['Order Date'] > '2020-01-01']
+
+    res = {}
+    res['orders_from_2020-01-01'] = df
+
+    df['product_details_bool'] = df['Product Details'].apply(
+        lambda x: 'GMDIS' in x)
+    ndf = df.loc[df['product_details_bool'] == True]
+
+    order_cols = [
+        'Order ID', 'Order Status', 'Order Date', 'Order Total (inc tax)',
+        'Gift Certificate Code', 'Gift Certificate Amount Redeemed'
+    ]
+
+    df = split_to_order_lines(ndf, order_cols)
     df['gmdis_bool'] = df['Product SKU'].apply(lambda x: 'GMDIS' in x)
     df = df.loc[df.gmdis_bool]
+    res['gmdis_order_lines'] = df
 
-    folder = 'results'
-    my_utils.create_directory([folder])
-    df.to_csv(folder + '/' + today + '_gdmis_orders.csv', index=False)
+    filename = '_gdmis_orders.xlsx'
+    my_utils.save_res_dict_to_xlsx(res, filename)
     return 0
 
 
